d65e30da4c6098804b3770a582fbf3c9f6a44f6a
Alexander Semenov <SemenovDev@yahoo.com>
2022-01-06 13:00:50 +0600

    Implement text converter

--- index.js
+++ index.js
@@ -3,30 +3,37 @@ class App extends React.Component {
     super(props);
     this.state = {
       input: '## Hello',
-      output: 'Hi'
     };
     this.handleChange=this.handleChange.bind(this);
+    this.updatePreview=this.updatePreview.bind(this);
   }
-  handleChange(event){
+
+  handleChange(event) {
     this.setState({
       input: event.target.value,
-    })
+    });
   }
+
+  updatePreview() {
+    document.getElementById('preview').innerHTML = marked.parse(this.state.input)
+  }
+  
   render() {
     return (
-      
       <div>
         <h1>Markdown Preveiwer</h1>
         <div>
           <h2>Markdown</h2>
           <textarea
             id='editor'
+            value={this.state.input}
             onChange={this.handleChange}
-          >{this.state.input}</textarea>
+            onKeyDown={this.updatePreview}
+          ></textarea>
         </div>
         <div>
           <h2>Preview</h2>
-          <div id='preview'>{this.state.output}</div>
+          <div id='preview'></div>
         </div>
       </div>
     );
