b2a0abd5de11246f263c8b7a085b40f4e3b7bcad
Alexander Semenov <SemenovDev@yahoo.com>
2022-01-07 13:55:37 +0600

    default preview

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      input: '## Hello',
-    };
-    this.handleChange=this.handleChange.bind(this);
-    this.updatePreview=this.updatePreview.bind(this);
   }
-
-  handleChange(event) {
-    this.setState({
-      input: event.target.value,
-    });
+  componentDidMount () {
+    document.getElementById('editor').value = placeholder;
+    this.updatePreview();
   }
 
   updatePreview() {
-    document.getElementById('preview').innerHTML = marked.parse(this.state.input)
+    let inputText = document.getElementById('editor').value;
+    let markedinputText = marked.parse(inputText);
+    let previewText = document.getElementById('preview');
+    previewText.innerHTML = markedinputText;
   }
   
   render() {
@@ -26,9 +22,7 @@ class App extends React.Component {
           <h2>Markdown</h2>
           <textarea
             id='editor'
-            value={this.state.input}
-            onChange={this.handleChange}
-            onKeyDown={this.updatePreview}
+            onKeyUp={this.updatePreview}
           ></textarea>
         </div>
         <div>
@@ -39,5 +33,51 @@ class App extends React.Component {
     );
   }
 };
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
 
 ReactDOM.render(<App/>, document.getElementById('root'));
