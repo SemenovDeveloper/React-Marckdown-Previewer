5ba2c6e1412885c830753f9cf20e2173ec0e937a
Alexander Semenov <SemenovDev@yahoo.com>
2022-01-14 14:42:02 +0600

    Ready

---
+++ README.md
@@ -0,0 +1,10 @@
+# Markdown Previewer
+
+## Parse markdown to html and preview it.
+
+## This project was created with:
+
+- [React JS](https://reactjs.org/)
+- [Sass](https://sass-lang.com/)
+- [Marked library](https://cdnjs.com/libraries/marked)
+- [Font Awesome Icons](https://fontawesome.com/)
--- index.js
+++ index.js
@@ -16,27 +16,25 @@ class App extends React.Component {
   }
   
   copyText() {
-      var copyText = document.getElementById("editor");
-      copyText.select();
-      copyText.setSelectionRange(0, 99999);
-      navigator.clipboard.writeText(copyText.value);
-      alert("Copied the text: " + copyText.value);
-  } 
+    let text = document.getElementById('editor').value;
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", text);    
+  }
+  
 
   render() {
     return (
       <div id="app-container">
-        <h1 id="header">Markdown Preveiwer</h1>
+        <h1 id="header">Markdown Previewer</h1>
         <div id="markdown-preview-container">
           <div id="markdown-container">
             <div id="toolbar-markdown">
               <h3>MARKDOWN</h3>
-              <i class="far fa-copy" onclick={this.copyText}></i>
+              <i className="far fa-copy" onClick={this.copyText}></i>
             </div>
             <textarea
               id='editor'
               onKeyUp={this.updatePreview}
-              class="form-control"
+              className="form-control"
             ></textarea>
           </div>
           <div id="preview-container">
@@ -46,55 +44,42 @@ class App extends React.Component {
             <div id='preview'></div>
           </div>
         </div>
-        <div class="footer"><a id="footer" target="_blank" href="https://github.com/SemenovDeveloper"><i class="fab fa-github-square"></i>by SemenovDeveloper</a></div>
+        <div className="footer"><a id="footer" target="_blank" href="https://github.com/SemenovDeveloper"><i className="fab fa-github-square"></i>by SemenovDeveloper</a></div>
       </div>
     );
   }
 };
 
-const placeholder = `# Welcome to my React Markdown Previewer!
+const placeholder = `# Here are some features
 
-## This is a sub-heading...
-### And here's some other cool stuff:
+# Heading 1
 
-Heres some code, \`<div></div>\`, between 2 backticks.
+## Heading 2
+
+**bold** text
+
+*italic* text
+
+\`Inline code\` with backticks
 
 \`\`\`
-// this is multi-line code:
-
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
+# This is a code block
+print '3 backticks'
 \`\`\`
 
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
+> This is a blockquote
 
-There's also [links](https://www.freecodecamp.org), and
-> Block Quotes!
+This is an unordered list:
+- apples
+- oranges
+- bananas
 
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | -------------
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
-
-
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
-
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+This is an ordered list:
+1. One
+2. Two
+3. Three  
+![Image](https://cdn-icons-png.flaticon.com/64/733/733609.png) 
+[This Markdown Previewer on GitHub](https://github.com/SemenovDeveloper/React-Marckdown-Previewer)
 `;
 
 
